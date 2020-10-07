a6f110eaa6a30cbd85c38d2c5e0a47cd0195bbce
Stirling Carter <stirling.h.carter@vanderbilt.edu>
2020-10-07 03:12:05 -0400

    ooo boy

--- house.py
+++ house.py
@@ -1,35 +1,58 @@
 import uuid
+import itertools
+MONTHS_IN_YEAR = 12
 
-SUPPLEMENTAL_INC_PER_MO = 1800*2 - 500 - 1500
-MOS_SI_REC = 0
-STARTING_CASH = 100000
+### FINANCIAL STARTING POINT
+SUPPLEMENTAL_INC_PER_MO = 1888*2 - 500 - 1500
+MOS_SI_REC = 36
+STARTING_CASH = 25000
+
+### USER CHOICE PARAMS
+
+# dynamic
+HOME_VALUE_TARGET = 160000 # 0
+MAX_HOUSES = 7 # 1
+REFI_CASH_MIN = 10000 # 2
+REFI_MOS_MIN = 12 # 3
+
+#[point],[ra,nge]
+DEFAULT_SNAPSHOT = [HOME_VALUE_TARGET,MAX_HOUSES,REFI_CASH_MIN,REFI_MOS_MIN]
+
+# static
+MIN_PURCHASE_GAP_MOS = 10
+REFI = 1 # 1 is true
+PAYOFF = 0 # 1 is true
+
+### RENT ADJUSTMENT PARAMS
+ADJUST_RENT_TO_MARKET = "ON_TURNOVER"
+RENT_ADJUST_PERIOD_MOS = 12 #PERIODICLY
+AVG_TURNOVER_MOS = 50 #ON_TURNOVER
+
+### MORTGAGE PARAMS
 MORTGAGE_LENGTHS = 30
-APR = .03
-APR_FACTOR = 1.93
-HOME_VALUE_TARGET = 160000
-DOWN_PAYMENT_PERCENTAGE = .2
 RI_YRLY_APPR = .03
 ETF_YRLY_DRAW = .04
-MONTHS_IN_YEAR = 12
-MIN_PURCHASE_GAP_MOS = 12
-MAX_HOUSES = 7
-ADJUST_RENT_TO_MARKET = "ON_TURNOVER" #ON_TURNOVER or PERIODICLY
-RENT_ADJUST_PERIOD_MOS = -1
-AVG_TURNOVER_MOS = 50
-RENT_PER_VALUE = .01
+APR = .03
+APR_FACTOR = 1.93
+DOWN_PAYMENT_PERCENTAGE = .2
+GENERAL_DOWN = DOWN_PAYMENT_PERCENTAGE * HOME_VALUE_TARGET
 
+### PROPERTY OPS
+RENT_PER_VALUE = .01
 CAPEX_PER_RENT = .10
 MAINT_PER_RENT = .10
 MANAGEMENT_PER_RENT =.10
-INSURANCE_PER_VALUE_PER_MO = .018 / 12
-PROPERTY_TAX_PER_VALUE_PER_MO = .0136 / 12
+INSURANCE_PER_VALUE_PER_MO = .018 / MONTHS_IN_YEAR
+PROPERTY_TAX_PER_VALUE_PER_MO = .0136 / MONTHS_IN_YEAR
 
+### LIMITS
 TARGET_MO_INC = 3000
-
+MAX_YEARS = 30
 
 class House:
 
     def __init__(self, cost, down):
+        self.mo_since_last_refi = 0
         self.appr_value = cost
         self.initial_value = cost
         self.rent = self.initial_value * RENT_PER_VALUE
@@ -62,6 +85,8 @@ class House:
             self.i_owed -= interest
         #acct for apr
         self.appr_value *= (1 + (RI_YRLY_APPR/MONTHS_IN_YEAR))#.04 is yrly RI ^
+        
+            
     
     def getNOI(self):
     
@@ -77,7 +102,7 @@ class House:
         return NOI
         
     def get_monthly_income(self):
-        if self.age_months % 12 == 0:
+        if self.age_months % MONTHS_IN_YEAR == 0:
             self.NOI = self.getNOI()
         return self.NOI
     
@@ -87,7 +112,15 @@ class House:
         print("VALUE: " + str(self.appr_value))
         print("BRINGIN IN MONTHLY: " + str(self.get_monthly_income()))
         print("\n")
-
+        
+    def resetMosSinceLastREFI(self):
+        self.mo_since_last_refi = 0
+            
+    def incMosSinceLastREFI(self):
+        self.mo_since_last_refi += 1
+    
+    
+    
     def refi(self):
         down = self.appr_value * DOWN_PAYMENT_PERCENTAGE
         out = self.appr_value - self.initial_value + self.equity - down
@@ -116,9 +149,55 @@ class House:
         else:
             return 100 / self.appr_value * self.p_owed
 
+def getRanges(snapshot):
+    ranges = []
+    for p in range(len(snapshot)):
+        expanded_range = []
+        r = snapshot[p] #[4,3,5]
+        
+        if len(r) == 1:
+            expanded_range = r # [4]
+            ranges.append(expanded_range)
+        else:
+            if len(r) == 2:
+                r.append(1)
+            for n in range(r[0],r[1],r[2]):
+                expanded_range.append(n)
+            ranges.append(expanded_range)
+            
+    return ranges
+        
+def getOptimum(snapshot_ranges):
+    ranges = getRanges(snapshot_ranges)
+    optimum_snapshot = []
+    optimum_time_mos = 100000000
+    optimum_uuid = 0
 
-def run():
+    # for [4,5,6],[3],[5,10],[1]
+    for point in itertools.product(ranges[0], ranges[1], ranges[2], ranges[3]):
+        SNAPSHOT = [point[0],point[1],point[0] * DOWN_PAYMENT_PERCENTAGE,point[3]]
+        ans_set = run(SNAPSHOT)
+        time_mos = ans_set[0]
+        u = ans_set[1]
+        if time_mos < optimum_time_mos:
+            optimum_snapshot = SNAPSHOT
+            optimum_time_mos = time_mos
+            optimum_uuid = u
+    
+    ans = {}
+    ans["optimum_snapshot"] = optimum_snapshot
+    ans["optimum_time_mos"] = optimum_time_mos
+    ans["optimum_uuid"] = optimum_uuid
 
+ 
+    return ans
+        
+def run(SNAPSHOT):
+    HOME_VALUE_TARGET = SNAPSHOT[0] # 0
+    MAX_HOUSES = SNAPSHOT[1] # 1
+    REFI_CASH_MIN = SNAPSHOT[2] # 2
+    REFI_MOS_MIN = SNAPSHOT[3] # 3
+    
     u = str(uuid.uuid1()).split("-")[0]
     print(u)
 
@@ -128,8 +207,10 @@ def run():
     houses = []
     months = 0
     mos_since_last_purchase = MIN_PURCHASE_GAP_MOS+1
-
-    while (monthly_income < TARGET_MO_INC and months < 360) and cash > 0:
+    months_target_met = 30000
+    time_set = False
+    while (monthly_income < TARGET_MO_INC or months < MAX_YEARS * MONTHS_IN_YEAR) and cash > 0:
+        
         if months < MOS_SI_REC:
             cash += SUPPLEMENTAL_INC_PER_MO
         months += 1
@@ -139,29 +220,32 @@ def run():
 
         for h in houses:
             h.step()
-            if h.equity + h.appr_value - h.initial_value > 2 * down and len(houses) < MAX_HOUSES:
-                print("\n\n\nREFIIIIIII\n\n")
-                cash += h.refi()
-        
-        if len(houses) == MAX_HOUSES:
-            for h in houses:
             
+        #REFI
+        if REFI == 1:
+            for h in houses:
+                down = h.appr_value * DOWN_PAYMENT_PERCENTAGE
+                out = h.appr_value - h.initial_value + h.equity - down
+                if out > REFI_CASH_MIN and len(houses) < MAX_HOUSES and h.mo_since_last_refi >= REFI_MOS_MIN:
+                    print("\n\n\nREFIIIIIII\n\n")
+                    h.resetMosSinceLastREFI()
+                    cash += h.refi()
+                else:
+                    h.incMosSinceLastREFI()
+
+        #PAYOFF
+        if PAYOFF == 1 and len(houses) == MAX_HOUSES:
+            for h in houses:
                 if h.p_owed != 0 and h.p_owed < cash:
                     print("PAID OFF")
-                    cash -= h.payOff()
+                cash -= h.payOff()
 
         cash += monthly_income
-        new_monthly_income = 0
-        for h in houses:
-            
-            new_monthly_income += h.get_monthly_income()
-        monthly_income = new_monthly_income + cash*ETF_YRLY_DRAW/MONTHS_IN_YEAR
         
-        print("\n")
-        print("CASH: " + str(cash))
-        print("MOS: " + str(months))
-        print("MONTHLY: " + str(monthly_income))
-        print("\n")
+        new_monthly_income = 0
+        
+        
+        
         mos_since_last_purchase += 1
         if cash > down and len(houses) < MAX_HOUSES and mos_since_last_purchase >= MIN_PURCHASE_GAP_MOS:
             print("PURCHASED")
@@ -169,45 +253,73 @@ def run():
             houses.append(House(HOME_VALUE_TARGET,down))
             cash -= down
             mos_since_last_purchase = 0
+        for h in houses:
+            new_monthly_income += h.get_monthly_income()
+            
+        monthly_income = new_monthly_income + cash*ETF_YRLY_DRAW/MONTHS_IN_YEAR
+        
+        
+        if monthly_income > TARGET_MO_INC:
+            if time_set == False:
+                print(months_target_met)
+                months_target_met = months
+                print(months_target_met)
+                time_set = True
+            
+        print("\n")
+        print("CASH: " + str(cash))
+        print("MOS: " + str(months))
+        print("MONTHLY: " + str(monthly_income))
+        print("\n")
+        
             
 
         
         if months%MONTHS_IN_YEAR == 0:
-            print("----------------------------------------------------------------------VVV-YEAR" + u + str(months//MONTHS_IN_YEAR)+"-VVV--------------------------------------------------------------")
+            print("----------------------------------------------------------------------VVV-YEAR" + str(months//MONTHS_IN_YEAR)+"-VVV--------------------------------------------------------------")
             for i in range(len(houses)):
                 print("\n")
                 print("HOUSE " + str(i) + ":")
                 houses[i].print_house()
-            print("----------------------------------------------------------------------^^^-YEAR"+str(months//MONTHS_IN_YEAR)+"-^^^------------------------------------------------------------")
+            print("--------------------------------------" + u + "-------------------------------^^^-YEAR"+str(months//MONTHS_IN_YEAR)+"-^^^------------------------------------------------------------")
         
     if cash <= 0:
         print("YA LOSE")
 
 
-    print("YEAR" + u)
+    print("----------------------------------------------------------------------VVV-FINAL-VVV--------------------------------------------------------------")
+    for i in range(len(houses)):
+        print("\n")
+        print("HOUSE " + str(i) + ":")
+        houses[i].print_house()
+    print("-----------------------------------------------" + u + "----------------------^^^-FINAL-^^^------------------------------------------------------------")
     
-    return [months, u]
+    return [months_target_met, u]
     
-run()
-opt = [0,0]
-opt_t = 100000000
-opt_u = 0
-for m in range(1,30):
-    MAX_HOUSES = m
-    for v in range(50000,1000000,10000):
-        HOME_VALUE_TARGET = v
-        ans_set = run()
-        t = ans_set[0]
-        u = ans_set[1]
-        if t < opt_t:
-            opt[0] = m
-            opt[1] = v
-            opt_t = t
-            opt_u = u
+
+
+
+
+
+
+
+
+
+
+
+
+###         HOME_VALUE_TARGET,MAX_HOUSES,REFI_CASH_MIN,REFI_MOS_MIN
+
+snapshot_ranges = [[75000,500000,10000],[1,4],[GENERAL_DOWN],[MONTHS_IN_YEAR]]
+
+            
+optimum = getOptimum(snapshot_ranges)
+        
 print("\n")
 print("\n")
 print("-----------------------------------FINAL RESULTS-----------------------------------")
 
-print("OPTIMUM N, V:" + str(opt))
-print("ID: " + u)
-print("MONTHS to $" + str(TARGET_MO_INC) + " PIPM: " + str(opt_t))
+
+
+print("OPTIMUM: " + str(optimum))
+
